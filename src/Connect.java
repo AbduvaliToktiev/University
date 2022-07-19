5995e27d4eec63192ade4b74e4f146c93c2e06b7
Admin <abdutokt2004>
2022-07-19 14:30:27 +0600

    HW_L43

---
+++ src/Connect.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Connect {
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String user = "postgres";
+    private final String password = "abdutokt2004";
+
+
+    public Connection connection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+}
